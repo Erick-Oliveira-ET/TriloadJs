b9a80274abe0e78acc4ac8727ab6beb270b971bb
Erick Oliveira <erickoliveirasystem@outlook.com>
2020-07-22 12:24:45 -0300

    Bug Fix on the mineserver command
    Fixed all bugs on the mineserver commands.
    - Need to improve UX with more responses from the bot

--- commands/mineserver.js
+++ commands/mineserver.js
@@ -32,86 +32,126 @@ module.exports = {
                 ])
 
                 await loginPage
-                    .waitForSelector('#nope > main > section > div.page-content.page-server > div.server-status > div.status.offline > div > span.statuslabel-label-container > span', {visible: true})
+                    .waitForSelector('#nope > main > section > div.page-content.page-server > div.server-status > div.status.offline > div > span.statuslabel-label-container > span', {visible: true, timeout: 5000})
                     .then((async () => {            
                         const disconnected = await loginPage.$eval('#nope > main > section > div.page-content.page-server > div.server-status > div.status.offline > div > span.statuslabel-label-container > span', el => el.textContent).catch(()=>{})
                         
                         if(disconnected.includes('Desconectado'))
                             serverRunning = false;
                             console.log("Entrou no if de desconectado " + serverRunning);
-                    })).catch(() => {console.log("Esperou por muito tempo e nao achou nada")})
-                
-                console.log("Trying things " + serverRunning !== false);
+                    })).catch(() => {console.log("Esperou por muito tempo em desconectado e nao achou nada")})
                 
                 if (serverRunning !== false) {
                     await loginPage
-                        .waitForSelector('#nope > main > section > div.page-content.page-server > div.server-status > div.status.online > div > span.statuslabel-label-container > span', {visible: true})
+                        .waitForSelector('#nope > main > section > div.page-content.page-server > div.server-status > div.status.online > div > span.statuslabel-label-container > span', {visible: true, timeout: 1500})
                         .then((async () => {            
                             const connected = await loginPage.$eval('#nope > main > section > div.page-content.page-server > div.server-status > div.status.online > div > span.statuslabel-label-container > span', el => el.textContent).catch(()=>{})
                             if(connected.includes('Conectado')){
                                 serverRunning = true;
                                 console.log("Entrou no if de conectado " + serverRunning);
                             }
-                        }))
+                        })).catch(() => {console.log("Esperou muito por conectado e não achou nada")})
                     
                 }
                 
                 if (args[0] === 'start') {
                     console.log("Entrou no if de start " + serverRunning)
-                    if (!serverRunning) {
-                        //Waits for start button appear to click it
-                        await Promise.all([ 
-                            await loginPage.waitForNavigation(),
-                            await loginPage.click('#start')
-                        ])
-        
-                        //Waits for close button on the modal appear to click it
-                        loginPage
-                            .waitForSelector('#nope > main > div > div > div > main > div > a.btn.btn-red', {visible: true})
-                            .then((async () => {
-                                await loginPage.click('#nope > main > div > div > div > main > div > a.btn.btn-red'); 
-                            }))
+                    if (serverRunning === false) {
+                        /*
+                        The function waitForSelector to be visible doesn´t work because when the 
+                        button is visible, the bot immediately click it, but it's so fast that the browser
+                        didn't finished load and doesn't notice the click. And the waitForURL doens't work
+                        because the URL doesn't change exactly (I think we're not send to another page but
+                        just the name of the URL change). Puppeteer click() method has a delay between 
+                        mousedown and mouseup but I thought it could fail if the page don't register 
+                        the pressed dow part
+                        To fix this problem, the bot wait for 5 seconds before clicking the start button 
+                        */
                         
-                        //After the countdown start, notify the user
-                        message.channel.send("O server irá abrir em, no mínimo, 5 minutos.\n"
-                        + "É possível acompanhar o tempo restante no status do server no minecraft.");
-                        
-                        //Waits for confirm button appear to click it
-                        loginPage
-                            .waitForSelector('#confirm', {visible: true, timeout: 0})
-                            .then((async () => { 
-                                await loginPage.click('#confirm');
-                                message.channel.send("O server está abrindo. Falta pelo menos 2 min.\n" + 
-                                "Se ninguém entrar em 3 min após abrir, o server será encerrado!!!");
-                            }))
+                        setTimeout(async () => {
+                            await loginPage
+                                .waitForSelector('#start', {visible: true})
+                                .then(async () => {
+                                    await loginPage.click('#start');
+                                })
+                                .catch(() => {console.log("Não clicou no botão de start");})
+                                
+                            await loginPage
+                                .waitForSelector('#nope > main > div > div > div > main > div > a.btn.btn-red', {visible: true})
+                                .then((async () => {
+                                    await loginPage.click('#nope > main > div > div > div > main > div > a.btn.btn-red'); 
+                                }))
+                                .catch(() => {console.log("Não fechou o modal");})
+                                
+                            //After the countdown start, notify the user
+                            message.channel.send("O server irá abrir em, no mínimo, 5 minutos.\n"
+                            + "É possível acompanhar o tempo restante no status do server no minecraft.");
+                            
+                            //Waits for confirm button appear to click it. 
+                            //Timeout: 0 set the waitForSelector() to wait indefinitely
+                            await loginPage
+                                .waitForSelector('#confirm', {visible: true, timeout: 0})
+                                .then((async () => { 
+                                    await loginPage.click('#confirm');
+                                    message.channel.send("O server está abrindo. Falta pelo menos 2 min.\n" + 
+                                    "Se ninguém entrar em 3 min após abrir, o server será encerrado!!!");
+                                }))
+                                .catch(() => {console.log("Não confirmou a abertura do server");})
+                            
+                            await browser.close();
+                        }, 1000)
                         
                     } else {
                         message.channel.send("Servidor já está aberto ou entre processos!!!");
+                        await browser.close();
                     }
                     
                     
                 } else if (args[0] === 'close') {
-                    if (serverRunning) {
-                        await loginPage.click('#stop');
-                        message.channel.send("O servidor está fechando.");
-                        serverRunning = false;
+                    if (serverRunning === true) {
+                        setTimeout(async () => {
+                            await loginPage
+                                .waitForSelector('#stop', {visible: true})
+                                .then((async () => { 
+                                    await loginPage.click('#stop');
+                                    message.channel.send("O servidor está fechando.");
+                                    serverRunning = false;
+                                
+                                }))
+                                .catch((e) => {console.log("Não confirmou o fechamento do server\n" + e);})
+                            
+                            await browser.close();
+                        }, 1000);
+                        
                     } else {
                         message.channel.send("Servidor já está fechado ou entre processos!!!");
-
+                        await browser.close();
                     }
         
                 
                 } else if (args[0] === 'restart') {
-                    if (!serverRunning) {
-                        message.channel.send("Não há como reiniciar o servidor pois o mesmo está fechado ou entre processos.");
-                    } else {
-                        await loginPage.click('#restart');
-                        message.channel.send("O servidor está reiniciando.")
-                    }
-                
+                    if (serverRunning === true) {
+                        setTimeout(async () => {
+                            await loginPage
+                                .waitForSelector('#restart', {visible: true})
+                                .then((async () => { 
+                                    await loginPage.click('#restart');
+                                    message.channel.send("O servidor está reiniciando.");
+                                }))
+                                .catch(() => {console.log("Não confirmou o reiniciamento do server");})
+                            
+                            await browser.close();
+
+                        }, 1000);
+                        
+                } else {
+                    message.channel.send("Não há como reiniciar o servidor pois o mesmo está fechado ou entre processos.");
+                    await browser.close();
+                }
+                    
                 }
 
-                await browser.close();
+               
             })();
 
 
