2db76f8aaacb047f95b5052312654245ebd260fb
Erick Oliveira <erickoliveirasystem@outlook.com>
2020-07-23 12:19:24 -0300

    play command
    The play command just plays one music

--- commands/play.js
+++ commands/play.js
@@ -1,66 +1,55 @@
 const Discord = require('discord.js');
 const { youtube_api } = require('../config/config.json');
 const ytdl = require('ytdl-core-discord');
-const {google} = require('googleapis');
-
-const yt = google.youtube({
-  version: 'v3',
-  auth: youtube_api.api_key
-});
-
+const ytsr = require('ytsr');
 
 const client = new Discord.Client();
 
 module.exports = {
-    name: 'play',
-    aliases: ["play"],
+    name: 'song',
+    aliases: ["song"],
 	description: 'Play music',
-    args: false,
+    args: true,
     usage: '<>',
-    execute(message, args) {
-        yt.search.list({part: 'id,snippet',q: args[0], maxResults: 1})
-            .then((result) => {
-                console.log(result.data.items);
-                message.channel.send("ok");
+    async execute(message, args) {
+        let songCommand = args.shift();
+        let songName = args.join(" ");
+        message.channel.send(songName + " " + songCommand);
+        let songURL;
+        await ytsr(songName)
+            .then(resp => {
+                songURL = resp.items[0].link
             })
+            .catch(()=>{});
 
+        console.log(songURL);
 
+        // Join the same voice channel of the author of the message
+        if (message.member.voice.channel.name) {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play(await ytdl(songURL), {type: 'opus'});
+            // async function play(connection, url) {
+            //     return connection.play(await ytdl(songURL), { type: 'opus' });
+            // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // // Join the same voice channel of the author of the message
-        // if (message.member.voice.channel.name) {
-        //     const connection = await message.member.voice.channel.join();
-        //     const dispacher = await play(connection, 'https://www.youtube.com/watch?v=4sgXyNdxjHg&list=PL_uO_IGxlxamAFSdRAg9x9V3C30pZ2NcD&index=2&t=0s');
-        //     async function play(connection, url) {
-        //         return connection.play(await ytdl(url), { type: 'opus' });
-        //     }
-        //     dispatcher.on(args[0], () => {
-        //         console.log('audio.mp3 is now playing!');
-        //     });
-    
-        //     dispatcher.on(args[0], () => {
-        //         console.log('audio.mp3 has finished playing!');
-        //     });
-    
-        //     // Always remember to handle errors appropriately!
-        //     dispatcher.on('error', console.error);
+            dispatcher.on('start', () => {
+                console.log('audio.mp3 is now playing!');
+            });
+                
+            dispatcher.on('finish', () => {
+                console.log('audio.mp3 has finished playing!');
+            });
             
-        // }
+            if (songCommand == 'stop') {
+                dispatcher.destroy();
+            }
+            
+    
+    
+            // Always remember to handle errors appropriately!
+            dispatcher.on('error', console.error);
+            
+        }
         
         
     }
